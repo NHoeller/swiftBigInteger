ef7d446e6006bdd646438145eadb93231d852dee
Nicolas Hoeller <Nicolas.Hoeller@gmx.eu>
2016-03-23 20:11:15 +0100

    Create isZero.swift

---
+++ isZero.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+extension BigInteger {
+
+    public func isZero() -> Bool {
+    
+        // check sign
+        if self.sign == 0 { return true }
+        
+        // if not null return false
+        return false
+
+    }
+
+}
