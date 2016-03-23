b0b04c5e70582c533c5969c32d4cf2b1694f335e
Nicolas Hoeller <Nicolas.Hoeller@gmx.eu>
2016-03-23 20:09:52 +0100

    Create isNegative.swift

---
+++ isNegative.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+extension BigInteger {
+
+    public func isNegative() -> Bool {
+    
+        // check sign
+        if self.sign == -1 { return true }
+        
+        // if not negative return false
+        return false
+    }
+}
