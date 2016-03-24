debafce44f08f9a17f238d7d74e70f31c7f2b621
Nicolas Hoeller <Nicolas.Hoeller@gmx.eu>
2016-03-24 21:56:01 +0100

    Update BigInteger.swift

--- BigInteger/BigInteger.swift
+++ BigInteger/BigInteger.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 public class BigInteger {
-
-    public enum BitSizes {
+    
+    public enum BitSizes: Int {
         case Int4 = 4
         case Int8 = 8
         case Int16 = 16
@@ -11,8 +11,10 @@ public class BigInteger {
     
     // define sign for BI (negative: -1 , zero: 0 , positive: 1)
     let sign: Int
+    
     // define bitSize taken from enum:BitSizes
     let bitSizePerChunk: Int
+    
     // defines maximum size per chunk
     // e.g. bitSizePerChunk = 4 -> maxSizePerChunk = 15 (binary 1111)
     // e.g. bitSizePerChunk = 8 -> maxSizePerChunk = 255 (binary 1111 1111)
@@ -20,18 +22,25 @@ public class BigInteger {
     
     // define the BigInteger
     let integerAsArray: [UInt32]
+    
+    public init(decimalAsString: String, isNegative: Bool = false, bitSize: BigInteger.BitSizes = BigInteger.BitSizes.Int32) {
 
-    public init(decimalAsString: String, isNegative: Bool = false, bitSize: BigInteger.BitSizes.Int32) {
         if decimalAsString == "" {
+
             integerAsArray = [0]
             sign = 0
-        }
-        else {
+
+        } else {
+
             integerAsArray = []
             // todo - transform string to array
             sign = isNegative ? -1 : 1
+
         }
+
         bitSizePerChunk = bitSize.rawValue
-        maxSizePerChunk = pow(2, bitSizePerChunk) - 1
+        maxSizePerChunk = UInt64(pow(Double(2), Double(bitSizePerChunk)) - 1)
+
     }
+
 }
